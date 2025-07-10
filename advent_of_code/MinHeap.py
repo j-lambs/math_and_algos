e7027c8a8eff537a40da7fef6f4564c25c33a5c8
j-lambs <jacklol87@yahoo.com>
2025-07-10 12:37:10 -0700

    Started working on Advent of Code 2024 Problem #1. Created LRNode and MinHeap classes

---
+++ advent_of_code/MinHeap.py
@@ -0,0 +1,6 @@
+class MinHeap:
+    def __init__(self, root) -> None:
+        self.root = root
+
+    def get_root(self):
+        return self.root
---
+++ advent_of_code/p1_dist_btwn_lists.py
@@ -0,0 +1,10 @@
+from MinHeap import MinHeap
+from LRNode import LRNode
+
+with open("p1_sample_input.txt") as f:
+  for line in f:
+    first_int = int(line[0])
+    second_int = int(line[4])
+    print(f'{first_int}, {second_int}')
+
+
